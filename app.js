79fe261b010b16daf326c27cc64e7a572842c643
NISHANT KETU <nketu06@gmail.com>
2020-09-12 18:38:34 +0530

    v1

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
