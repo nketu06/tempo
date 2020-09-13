742c01743926fe4859546da620e567a0cfd66d61
NISHANT KETU <nketu06@gmail.com>
2020-09-13 19:20:56 +0530

    added main navigation bar

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
 
 function myFunction() {
+
   var x = document.getElementById("myTopnav");
   if (x.className === "navbar") {
     x.className += " responsive";
   } else {
     x.className = "navbar";
   }
+}
+
+function navfunction(){
+  var y = document.getElementById("myMainnav");
+  if (y.className === "mainnav m-row") {
+    y.className += " navresponsive";
+  console.log(y.className)
+  } else {
+    y.className = "mainnav m-row";
+  }
+
 }
